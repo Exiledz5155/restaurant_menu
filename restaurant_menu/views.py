6e88b87653e3eff139841a0ffc21eaab09e5bbae
Danny Bui <Dannybui5155@gmail.com>
2023-09-05 11:23:11 -0500

    implemented About generic List view, modified base html to point to correct URL, updated url patterns

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -16,3 +16,15 @@ class MenuList(generic.ListView):
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
+
+class About(generic.ListView):
+    # queryset = Item.objects.order_by("meal")
+    template_name = "about.html"
+
+    # get query set override
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        term = self.kwargs.get('term')
+        if term:
+            return queryset.filter(body__icontains=term)
+        return queryset.none()
